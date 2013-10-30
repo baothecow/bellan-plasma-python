5029485ad203e57eb019bdc185a9f0226c230615
Bao <baoshock@gmail.com>
2013-10-29 19:46:43 -0700

    added vme_basic_2d_plot for generic plotting of vme readouts

--- testVME.py
+++ testVME.py
@@ -1,7 +1,7 @@
 ## Testing agnus's IO library.
 
 from file_io_lib import readVME
-from vme_plot import vme_plot_current
+from vme_plot import vme_basic_2d_plot
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,7 +13,8 @@ foldername = '2013.03.14\\'
 
 shotnum = ['570', '571', '572']
 
-title = 'This is my plot'
+title =  'This is my plot'
+ytitle = 'Current (kA)'
 rows=3
 
 
@@ -22,7 +23,7 @@ for counter in range(0, len(shotnum)):
     constructor = 'vi_t2ch13_' + shotnum[counter] + '.dat'
     filename = root + foldername + constructor
     data = readVME(filename, rows=rows)
-    vme_plot_current(data, style_counter=counter)
+    vme_2d_plot(data, style_counter=counter, ytitle=ytitle)
 
 
 plt.figtext(.5,.85,'Shot(s): ' + ", ".join(shotnum), fontsize=10,ha='center')
--- vme_plot.py
+++ vme_plot.py
@@ -38,6 +38,34 @@ def vme_plot_current(
     plt.ylim(ylim)
 
 
+## Plots VME data.
+def vme_basic_2d_plot(
+        data,
+        style_counter = 0,                # used to determine specific colors.
+        title = '2D Plot vs Time',
+        xtitle = 'Time (' + '$\mu$' + 's)',
+        ytitle = '',
+        xlim = [10, 30],
+        ylim = [-40, 40],
+        smoothing_window = 50):
+    """ Basic 2D plotting of a single quantity vs time """
+
+    time = data[0, :]
+    current = data[2, :]
+
+    plot_style = color_array[style_counter] + style_array[style_counter]
+
+    ## Plot the raw version using a thin line. 
+    plt.plot(time, current, plot_style, linewidth=0.5)
+    ## Plot the smoothed version using a thicker line.
+    plt.plot(time, smooth(current, window_len=smoothing_window), plot_style,
+             linewidth=2)
+    plt.title(title)
+    plt.ylabel(ytitle)
+    plt.xlabel(xtitle)
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+
 
 ### Some colors and styles.
 
