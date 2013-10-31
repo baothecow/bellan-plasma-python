3cab6a498285d13933d572b086111802823d715b
Bao <baoshock@gmail.com>
2013-10-31 01:24:45 -0700

    Cleaned up idl_support and implemented get_shot_date
    * Cleaned up the formatting found within idl_support.py.
    * Converted the script/static version of calling IDL's foldername pro
      to a dynamic version.
    * Call get_shot_date(shotnum) to have it return the date corresponding to
      the shot.

--- idl_support.py
+++ idl_support.py
@@ -1,27 +1,80 @@
-## This file is motivated by an article found at:
-## http://brainimaging.waisman.wisc.edu/~oakes/idl/idl_from_python.html
+""" Contains functions which allow python-idl interactivity 
+
+
+    Example: get_shot_date(shotnum) returns a string with date.
+
+
+
+"""
 
 import subprocess
+import os
 
-envir = {'PATH': 'C:\\Program Files\\ITT\\IDL\\IDL81\\bin\\bin.x86_64\\'}
+ENVIR = {'PATH': 'C:\\Program Files\\ITT\\IDL\\IDL81\\bin\\bin.x86_64\\'}
+PY_LIB_PATH = 'C:\\Users\\Bao\\Documents\\GitHub\\bellan-plasma-python\\'
+IDL_LIB_PATH = 'G:\\programs\\idl\\singleloop_lib\\'
 
-filepath = 'C:\\Users\\Bao\\Documents\\GitHub\\bellan-plasma-python\\'
 
-subprocess.Popen('echo "Hello world!"', shell=True)
+def gen_read_foldername_pro(shotnum):
+    """ Gen temp file to call IDL's foldername
+    
+        shotnum - integer corresponding to a shot number.
+    
+    """
+    
+    f = open(PY_LIB_PATH + 'temp_idl_file.pro','w')
+    f.write(".compile " + IDL_LIB_PATH + "foldername.pro\n")
+    f.write("folder = \"\"\n")
+    f.write("foldername, " + str(shotnum) + ", folder, verbal=1\n")
+    f.write("wait, 0.01\n")       # Process may terminate too early otherwise.
+    f.close()
 
-subp = subprocess.Popen("idl -e \"@"+filepath+"test.pro\"" ,
+
+def get_idl_lib_path():
+    """ Returns the IDL_LIB_PATH variable """
+    return IDL_LIB_PATH
+    
+    
+def run_temp_idl_file_pro():
+    """ Use subprocess to run temp_idl_file.pro if one exists """
+    
+    subp = subprocess.Popen("idl -e \"@" + PY_LIB_PATH + "temp_idl_file.pro\"",
         stderr = subprocess.PIPE, stdout = subprocess.PIPE, shell = True, 
-        env=envir, bufsize=1)
+        env=ENVIR)
+    
+    # Extracts the output.
+    (idl_stdout, idl_stderr) = subp.communicate()
 
-(idl_stdout, idl_stderr) = subp.communicate()
+    # Split the string into individual lines and print the last line.
+    stringarr = idl_stdout.split()
+    print stringarr[-1]
+    
+    
+def get_shot_date(shotnum):
+    """ Returns the date associated with a shot by using foldername.pro """
+    
+    # Generates a temp .pro to read foldername and extract the shot date.
+    gen_read_foldername_pro(shotnum)        
+    run_temp_idl_file_pro()
+    os.remove(PY_LIB_PATH + "temp_idl_file.pro")
+    
 
+get_shot_date(500)
+    
 
-#print(idl_stdout)   # for python 2.4, use "print idl_stdout" instead
-
-# Make a copy.  Manipulating this output directly messes things up for
-# some mysterious reason.
-idl_output = idl_stdout
-
-# Split the string into individual lines.
-stringarr = idl_output.split()
-print stringarr[-1]
+#
+## Remove the temporary file
+#
+#
+#
+##print(idl_stdout)   # for python 2.4, use "print idl_stdout" instead
+#
+## Make a copy.  Manipulating this output directly messes things up for
+## some mysterious reason.
+#idl_output = idl_stdout
+#
+## Split the string into individual lines.
+#stringarr = idl_output.split()
+#print stringarr[-1]
+#
+#
--- test.pro
+++
@@ -1,7 +0,0 @@
-.compile G:\programs\idl\singleloop_lib\foldername.pro
-
-folder = ""
-
-foldername, 755, folder, verbal=1
-
-wait, 0.1   ; Add wait else scripts may terminate too early.
