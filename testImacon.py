bb11d859a0230bd0db0e8aadfa6b32f3e46e25fe
Bao <quocbaoha@gmail.com>
2014-07-30 06:26:20 -0700

    minor updates to imacon analysis files

--- testImacon.py
+++ testImacon.py
@@ -7,11 +7,21 @@ Created on Mon Jan 06 23:20:57 2014
 
 ### Script files for running imacon.
 
-from imacon_analyze import *
 
-framenum = 4
-filepaths = get_file_paths(folderpath=INITIAL_DIR)
+# Script to make movie from reduced imacon file.
+from imacon_analyze import make_movie_from_reduced_png
 
-for path in filepaths:
-    im = imacon_choose_frame(path, framenum)
-    im.convert('RGB').save(imacon_generate_new_output_path(path, extra='_frame' + str(framenum)))
+
+
+
+
+
+### Scripts for extracting a specific frame from a list of imacon images.
+#from imacon_analyze import imacon_choose_frame, get_file_paths, imacon_generate_new_output_path, INITIAL_DIR
+#
+#framenum = 4
+#filepaths = get_file_paths(folderpath=INITIAL_DIR)
+#
+#for path in filepaths:
+#    im = imacon_choose_frame(path, framenum)
+#    im.convert('RGB').save(imacon_generate_new_output_path(path, extra='_frame' + str(framenum)))
