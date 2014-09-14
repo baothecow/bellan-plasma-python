a5ce247b634e73608778a8c5b975d94c0eb946ec
Bao <quocbaoha@gmail.com>
2014-09-14 11:21:15 -0700

    integrating with some hall stuff that is already implemented in vme_analyze.py

--- hall_analyze.py
+++ hall_analyze.py
@@ -2,6 +2,16 @@
 """
 Created on Fri Sep 12 11:05:36 2014
 
+
+
+
+Note: There is already a method of reading Hall sensor data from vme_analyze but does
+not intepret the location of the hall sensors and thus introduces artificial transients
+which are then removed.
+
+It also doesn't use a polarization matrix.
+
+
 @author: bao
 """
 
@@ -11,6 +21,7 @@ import matplotlib.pyplot as plt
 import os
 from file_io_lib import pickle_read, pickle_dump
 from cookb_signalsmooth import smooth
+from vme_analyze import get_hall_calibration_matrix
 
 
 def get_hall_date(shotnum):
@@ -57,8 +68,10 @@ def read_hall_file(filepath):
     
     fin = open(filepath,'rb')
     
-    location = ar.array('f')
-    location.fromfile(fin, 3)
+    #location = ar.array('f')
+    #location.fromfile(fin, 3)
+    
+    location = [1, 2, 3]
 
     array = ar.array('f')
     array.fromfile(fin, num_channel*points)    
@@ -113,25 +126,6 @@ def trim_hall_data(data, ntrim=5):
     # Applys the appropriate smoothing and return every other 2**ntrim points of the array column.
     return np.apply_along_axis(my_smooth, 1, data)[:, 1::2**ntrim]
     
-
-    
-def get_calibration_matrix(sensor):
-    """ Returns a calibration matrix """
-    if sensor == 'A':
-        return [[0.0780625, -0.00390558, 0.00189126], [-0.000338635, 0.0876258, -0.000521402], [0.00361111, 0.00381362, 0.0860203]] # sensor/board: A/A
-    elif sensor == 'B':
-        return [[0.0829776, -0.00337371, 0.00172883], [0.00353229, 0.0868474, -0.00203384], [0.00109795, 0.00886673, 0.0836279]] # sensor/board: B/B
-    elif sensor == 'C':
-        return [[0.0835019, 0.00166185, 0.00697365], [-0.00151638, 0.0906578, -0.00237361], [0.00210450, 0.00484992, 0.0845391]] # sensor/board: C/C
-    elif sensor == 'D':
-        return [[0.0834475, -0.00148635, -0.00390268], [0.00917581, 0.0783704, -0.505646e-005], [0.0170444, -0.000291056, 0.0821908]] # sensor/board: D/D
-    elif sensor == 'E':
-        return [[0.0896002, -0.00723647, -0.00446408], [0.00451907, 0.0859051, 0.00257616], [0.00530865, 0.00425686, 0.0846567]] # sensor/board: E/E
-    elif sensor == 'F':
-        return [[0.0855627, 0.000342191, -0.000336037], [0.00682840, 0.0817025, 0.000688643], [0.00186146, 0.00246870, 0.0822300]] # sensor/board: F/F
-    else:
-        print 'Uncalibrated sensor or sensor does not exist!'
-        return [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         
 def get_polarity_matrix(x, y, z):
     """
@@ -152,7 +146,7 @@ def calc_hall_for_shots(shotnums, sensor, polarization = [-1, -1, -1]):
     for shot in shotnums:
         location_list.append(read_hall_position(generate_hall_filepath(shot, sensor)))
         foo = trim_hall_data(read_hall_data(generate_hall_filepath(shot, sensor)))
-        cal_matrix = get_calibration_matrix(sensor)
+        cal_matrix = get_hall_calibration_matrix(sensor)
         pol_matrix = get_polarity_matrix(polarization[0], polarization[1], polarization[2])
         B = np.dot(cal_matrix, np.dot(pol_matrix, foo[1:4]))
         data_list.append(np.concatenate((foo[0:1], B[0:3]), axis=0))
@@ -169,7 +163,7 @@ def save_hall_to_file(location_list, data_list, path = 'E:\\data\\singleloop\\si
     pickle_dump(location_list, filename, path)
     pickle_dump(data_list, datafn, path)
     
-def read_hall_for_shots_from_file(path):
+def read_hall_from_file(path):
     
     
     location = pickle_read(path + 'location.pickle')
@@ -180,28 +174,31 @@ def read_hall_for_shots_from_file(path):
     
 
 sensor = 'A'
-start = 358
-shot_range = range(start, start+10)
+start = 303
+shot_range = range(start, start+3)
 
 
 
 
 (location_list, data_list) = calc_hall_for_shots(shot_range, sensor)
-save_hall_to_file(location_list, data_list)
+#save_hall_to_file(location_list, data_list)
 
 
 
-path = 'E:\\data\\singleloop\\singleloop_VME\\hall\\'
-(location_list, data_list) = read_hall_for_shots_from_file(path)
+#path = 'E:\\data\\singleloop\\singleloop_VME\\hall\\'
+#(location_list, data_list) = read_hall_from_file(path)
 
-plt.figure()
+#plt.figure()
+#
+#for data in data_list:
+#    plt.plot(data[0], data[1], 'g')
+#    plt.plot(data[0], data[2], 'b')
+#    plt.plot(data[0], data[3], 'r')
 
-for data in data_list:
-    plt.plot(data[0], data[1], 'g')
-    plt.plot(data[0], data[2], 'b')
-    plt.plot(data[0], data[3], 'r')
+#shot_array = (np.array([shot_range])).T
+#print np.concatenate((shot_array, location_list), axis=1)
 
-print np.array(location_list)
+
     
     
 #sensor_locations = list()
